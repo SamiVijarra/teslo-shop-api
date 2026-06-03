49871acdccb216f4ff2f7a4e7b20d9b4ce9a579a
Sami <samivijarra1@gmail.com>
2026-06-03 00:34:28 -0300

    feat:update product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -43,8 +43,14 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id,
+      ...updateProductDto,
+    });
+    if (!product)
+      throw new BadRequestException(`Product with id ${id} not found`);
+    return await this.productRepository.save(product);
   }
 
   async remove(id: string) {
