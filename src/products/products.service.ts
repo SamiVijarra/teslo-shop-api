c286f50cb81d864c40c9238cba76e1403bc41d34
Sami <samivijarra1@gmail.com>
2026-06-06 00:12:38 -0300

    feat: query runner transactions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -97,10 +97,24 @@ export class ProductsService {
       throw new NotFoundException(`Product with id ${id} not found`);
 
     const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     try {
-      await this.productRepository.save(product);
-      return product;
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+        product.images = images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        );
+      }
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      //await this.productRepository.save(product);
+      return this.findOnePlain(id);
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       this.handleDBExceptions(error);
     }
   }
