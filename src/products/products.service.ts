2c14a44fb42ac0af8e6cc65dddac0026df41bb3b
Sami <samivijarra1@gmail.com>
2026-06-05 23:36:46 -0300

    feat: add find one plain

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -46,10 +46,15 @@ export class ProductsService {
 
   async findAll(paginationDto: PaginationDto) {
     const { limit = 10, offset = 0 } = paginationDto;
-    return await this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
+      relations: { images: true },
     });
+    return products.map((product) => ({
+      ...product,
+      images: (product.images ?? []).map((img) => img.url),
+    }));
   }
 
   async findOne(term: string) {
@@ -57,17 +62,26 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
       product = await queryBuilder
         .where('UPPER(title) =:title or slug =:slug', {
           title: term.toUpperCase(),
           slug: term.toLowerCase(),
         })
+        .leftJoinAndSelect('prod.images', 'prodImages')
         .getOne();
     }
     if (!product)
       throw new NotFoundException(`Product with term ${term} not found`);
     return product;
+  }
+
+  async findOnePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return {
+      ...rest,
+      images: images.map((img) => img.url),
+    };
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
