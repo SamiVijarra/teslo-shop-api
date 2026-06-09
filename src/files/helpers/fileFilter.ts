2b9d4eb153531c014e6674a89cc505043981f8df
Sami <samivijarra1@gmail.com>
2026-06-08 23:17:23 -0300

    feat: add save filesystem

--- src/files/helpers/fileFilter.ts
+++ src/files/helpers/fileFilter.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 export const fileFilter = (
   req: Express.Request,
   file: Express.Multer.File,
   callback: Function,
 ) => {
-  if (!file) {return callback(new Error('File is empty'), false);
+  if (!file) {
+    return callback(new Error('File is empty'), false);
   }
 
   const fileExtension = file.mimetype.split('/')[1];
