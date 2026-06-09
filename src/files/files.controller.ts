64466526ca4969013725b31ec9a1a1d591a6fd7c
Sami <samivijarra1@gmail.com>
2026-06-09 00:31:22 -0300

    feat: add secure url

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -13,10 +13,14 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer, fileFilter } from './helpers';
 import express from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -44,7 +48,7 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
-    const secureUrl = `${process.env.HOST_API}/files/product/${file.filename}`;
-    return { fileName: file.originalname, secureUrl };
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+    return { secureUrl };
   }
 }
