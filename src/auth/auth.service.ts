d502e6a0d2f7b51ba658e03e7ca352f1c68f8d58
Sami <samivijarra1@gmail.com>
2026-07-10 20:20:31 -0300

    feat: add login users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,13 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 @Injectable()
 export class AuthService {
@@ -30,6 +31,21 @@ export class AuthService {
       this.handleDBErrors(error);
     }
   }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user || !user.password)
+      throw new UnauthorizedException('Credentials are not valid');
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid');
+    delete user.password;
+    return user;
+  }
+
   private handleDBErrors(error: any): never {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (error.code === '23505') {
