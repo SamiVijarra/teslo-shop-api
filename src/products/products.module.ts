da556d8b8b35770d360ff62ed51ad876cc9ad8f0
Sami <samivijarra1@gmail.com>
2026-07-16 19:59:01 -0300

    feat: add auth modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from 'src/auth/auth.module';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
