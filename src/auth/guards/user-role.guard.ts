149da32b4cf6cb58571bef57bdd47976a716b0f2
Sami <samivijarra1@gmail.com>
2026-07-15 21:31:29 -0300

    feat: add user role guard

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -1,5 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 
 import { Observable } from 'rxjs';
 
@@ -13,7 +23,29 @@ export class UserRoleGuard implements CanActivate {
       'roles',
       context.getHandler(),
     );
-    console.log({ validRoles });
-    return true;
+
+    if (!validRoles) {
+      return true;
+    }
+
+    if (validRoles.length === 0) {
+      return true;
+    }
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role: [${validRoles}]`,
+    );
   }
 }
